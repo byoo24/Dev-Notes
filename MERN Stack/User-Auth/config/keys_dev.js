ed8762fc83e9a0d3aa267c5b4d7c12a175a4dc37
byoo24 <byoo.dev@gmail.com>
2019-08-07 22:35:27 -0400

    added a comment

--- MERN Stack/User-Auth/config/keys_dev.js
+++ MERN Stack/User-Auth/config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds123456.mlab.com:12345/databasename',
-    secretOrKey: 'secret'
+    secretOrKey: 'secret' // use a random key generator
 }
